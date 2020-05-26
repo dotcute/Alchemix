9d50e6c7748bdf0e7205785da917615bcf50b676
muno9748 <58895791+muno9748@users.noreply.github.com>
2020-05-26 11:46:23 +0900

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import express, { Application, Request, Response } from 'express';
 import path from 'path';
 const app: Application = express();
-app.use('/assets', express.static(path.join(__dirname, 'assets')));
+app.use('/src', express.static(path.join(__dirname, 'src')));
 
 app.get('/', (req: Request, res: Response) => {
   res.sendFile(path.join(__dirname, 'index.html'));
